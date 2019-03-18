c2c22c9b54f31c59a62a042a34041832f4c78953
camilo andres ibarra yepes <cibarra@grupoalto.com>
2019-03-18 11:52:43 -0500

    init

---
+++ UITabBar/UITabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBar
+//
+//  Created by camilo andres ibarra yepes on 3/18/19.
+//  Copyright © 2019 camilo andres ibarra yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
